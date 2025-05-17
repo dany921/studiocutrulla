8afabfeeb45ef83eebeba02bfcbcab5b8064bde0
dany921 <daniloceraso@gmail.com>
2025-05-17 10:00:11 +0200

    Update vite.config.ts
    Primo deploy funzionante del sito con admin Decap CMS configurato correttamente

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,21 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import copy from 'rollup-plugin-copy';
+import path from 'path';
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
+export default defineConfig({
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+    copy({
+      targets: [
+        { src: 'admin/config.yml', dest: 'dist/admin' }
+      ],
+      hook: 'writeBundle'
+    }),
+  ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-}));
+});
